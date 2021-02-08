ab53515a9ea5f62f89399ac8b12ac4374f831f59
Tepozyolca <71712439+Tepozyolca@users.noreply.github.com>
2021-02-08 14:19:34 +0100

    Add files via upload

--- Script requêtes Green village.sql
+++ Script requêtes Green village.sql
@@ -1,10 +1,10 @@
 -- Script requêtes
 
-SELECT commande.com_id AS 'numéro de commande', commande.com_date as 'date de commande', clients.cli_id as 'numéro client', clients.cli_nom as 'nom du client', clients.cli_adresse as 'adresse du client', clients.cli_contact as 'détails contact du client', livraison.liv_adresse as 'adresse de livraison', facture.fac_adresse as 'adresse de facturation'
-FROM commande
-    JOIN facture ON facture.fac_com_id = commande.com_id
-	JOIN clients ON clients.cli_id = commande.com_cli_id
-	JOIN livraison ON livraison.liv_com_det_id = com_det_id
+SELECT commande.com_id, commande.com_date as 'date de commande', clients.cli_id as 'numéro client', clients.cli_nom as 'nom du client', clients.cli_adresse as 'adresse du client', clients.cli_contact as 'détails contact du client', livraison.liv_adresse as 'adresse de livraison', facture.fac_adresse as 'adresse de facturation'
+FROM commande 
+	JOIN clients    
+WHERE commande.com_cli_id = clients.Cli_id 
+ORDER BY `com_id` ASC
 
 SELECT sum(produits.pro_prix_HT*commande_details.com_det_quantite) as 'total hors taxes', sum(produits.pro_prix_HT*commande_details.com_det_quantite*commande_details.com_det_taxe/100) as 'TVA', sum(produits.pro_prix_HT*commande_details.com_det_quantite*(1+commande_details.com_det_taxe/100)) as 'total TTC'
 FROM produits
